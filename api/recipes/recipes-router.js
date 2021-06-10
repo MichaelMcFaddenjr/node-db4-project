5b3c756974680bc9dd25df47459b55511738449a
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-06-10 17:53:30 -0600

    wip

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,21 +1,21 @@
-const router = require('express').Router()
-const Recipe = require('./rescipes-model')
 
+const router = require("express").Router();
+const Recipe = require("./recipes-model");
 
-router.get('/:recipes_id', (req, res, next) => {
-  Recipe.getRecipeById(req.params.id)
-    .then(resource => {
-      res.status(200).json(resource)
+router.get("/:recipe_id", (req, res, next) => {
+  Recipe.getRecipeById(req.params.recipe_id)
+    .then((resource) => {
+      res.status(200).json(resource);
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
-router.use((err, req, res, next) => { //eslint disable line
+router.use((err, req, res, next) => { // eslint-disable-line
   res.status(500).json({
-    customMessage: 'something went wrong inside the recipes router',
+    customMessage: "something went wrong inside the recipes router",
     message: err.message,
-    stack: err.stack
-  })
-})
+    stack: err.stack,
+  });
+});
 
-module.exports = router
+module.exports = router;
--- api/recipes/rescipes-model.js
+++
@@ -1,5 +0,0 @@
-function getRecipeById(recipe_id) {
-  return Promise.resolve('awesome')
-}
-
-module.exports = { getRecipeById }
